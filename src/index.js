0eafb343f10c7ead13f77ce0e683fea2ac7ee5db
tyrone-offshorly <188826837+tyrone-offshorly@users.noreply.github.com>
2024-12-12 14:23:22 +0800

    Implement working form for project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import Project from "./components/Project.js";
 import Task from "./components/Task";
 import renderSidebar from "./components/Sidebar.js";
-(function() {
+
+const main = (function() {
   const projects = [];
 
   projects.push(new Project("default"));
 
   renderSidebar();
+
+
+  const addProject = function(project) { 
+    projects.push(project);
+    console.log(projects);
+  }
+
+  return {addProject};
 })();
+
+export { main };
